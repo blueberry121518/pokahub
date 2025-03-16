18947ede6e09d69e862dfca8e723fbdb8937d51a
Alvin Tan <alvinttan1215@gmail.com>
2025-03-16 15:06:27 -0700

    Light refactoring

--- Flask/app/services/user_services.py
+++ Flask/app/services/user_services.py
@@ -94,6 +94,25 @@ def database_user_exists(username) -> bool:
     else:
         return False
     
+import os
+from dotenv import load_dotenv
+
+def generate_token(username):
+    # Retrieve secret key
+    load_dotenv()
+    SECRET_KEY = os.getenv("SECRET_KEY")
+
+    # Generate token
+    expiration_time = datetime.datetime.utcnow() + datetime.timedelta(hours=1)
+    token = jwt.encode(
+        {"username": username, "exp": expiration_time},
+        SECRET_KEY,
+        algorithm="HS256"
+    )
+
+    return token
+
+
 
     
 
